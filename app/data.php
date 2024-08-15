ea20da6229ab9cd36eb223cc8007f58ab08a9f54
omarfaruk477 <omarfaruk477@gmail.com>
2024-08-15 18:30:13 +0600

    some update

---
+++ app/data.php
@@ -0,0 +1,129 @@
+<?php
+
+
+
+// header top
+$headerTop = [
+    "email"     => "omarfaruk477@gmail.com",
+    "phpne"     => "01721625268",
+    "topIcon"   => [
+            "twitter"   => "F",
+            "facebook"  =>  "A",
+            "g+"        =>  "R",
+            "linkdin"   =>  "U",
+            "youtube"   =>  "K"
+    ]
+];
+
+
+
+//LOGO
+$logo = [
+    "img"  => "https://th.bing.com/th/id/OIP.MufyKoJGKmFvtB52HGfh5gHaDL?w=2109&h=906&rs=1&pid=ImgDetMain"
+];
+
+$logoText = [
+    "text"  => "The best resturent in bangladesh"
+];
+
+// NAV BAR 
+$navBar = [
+    "title1"     =>  "Menu1",
+    "title2"     =>  "Menu2",
+    "title3"     =>  "Menu3",
+    "title4"     =>  "Menu4",
+    "title5"     =>  "Menu5",
+    "title6"     =>  "Menu6",
+    "title7"     =>  "Menu7",
+    "title8"     =>  "Menu8",
+];
+
+// SLIDERs
+$sliders = [
+    [
+        "slider_img"     =>  "https://fthmb.tqn.com/KlFiJ2R4qWs2yVZy3d9aqGv6KsM=/2116x1417/filters:fill(auto,1)/All-American-Sliders-58a72ba65f9b58a3c95db929.jpg",
+        "slider_desc"    => [
+                            "title"   => "Slider Title 1",
+                            "dec"     => "Slider Description 1"
+        ]
+    ],
+    [
+        "slider_img"     =>  "https://mambeno.co.uk/wp-content/uploads/2016/08/Sliders.jpg",
+        "slider_desc"    => [
+                            "title"   => "Slider Title 2",
+                            "dec"     => "Slider Description 2"
+        ]
+    ],
+    [
+        "slider_img"     =>  "https://th.bing.com/th/id/OIP.qt_aCx7FDUOXgovKVniHWQAAAA?rs=1&pid=ImgDetMain",
+        "slider_desc"    => [
+                            "title"   => "Slider Title 3",
+                            "dec"     => "Slider Description 3"
+        ]
+    ]
+];
+
+$bannerText = [
+    "title"  => "Best food for you",
+    "desc"  => "Best food for you"
+];
+
+
+
+
+// Welcome section
+$wlComeSec = [
+    "title"         => "Amar Sonar Bangla",
+    "seb_title"     => "Ami tomay valobashi. Chirodin tomar akash tomar batas"
+];
+  
+
+$foodCardSec = [
+    [
+        "img_title"     => "Menu Card 1",
+        "img"           => "https://img.menulist.menu/storage/media/company_gallery/43513401/conversions/contribution_gallery.jpg",
+        "img_desc"      => "Amar sonam bangla, ami tomay valobashi",
+        "btn"           => "Read More.."
+    ],
+    [
+        "img_title"     => "Menu Card 2",
+        "img"           => "https://enam-cdn.carta.menu/storage/media/company_gallery/80096874/conversions/contribution_gallery.jpg",
+        "img_desc"      => "Amar sonam bangla, ami tomay valobashi",
+        "btn"           => "Read More.."
+    ],
+    [
+        "img_title"     => "Menu Card 3",
+        "img"           => "https://th.bing.com/th/id/OIP.3ApxjwQUwQHkZQzOO9ENDgHaFj?w=245&h=184&c=7&r=0&o=5&pid=1.7",
+        "img_desc"      => "Amar sonam bangla, ami tomay valobashi",
+        "btn"           => "Read More.."
+    ],
+    [
+        "img_title"     => "Menu Card 4",
+        "img"           => "https://th.bing.com/th/id/OIF.95VmyRVQFqkbpp0XPQ5oLg?w=177&h=180&c=7&r=0&o=5&pid=1.7",
+        "img_desc"      => "Amar sonam bangla, ami tomay valobashi",
+        "btn"           => "Read More.."
+    ],
+    [
+        "img_title"     => "Menu Card 5",
+        "img"           => "https://th.bing.com/th/id/OIP.DllECGLsryhBm4L04FfbHwAAAA?rs=1&pid=ImgDetMain",
+        "img_desc"      => "Amar sonam bangla, ami tomay valobashi",
+        "btn"           => "Read More.."
+    ],
+    [
+        "img_title"     => "Menu Card 6",
+        "img"           => "https://th.bing.com/th/id/OIP.DllECGLsryhBm4L04FfbHwAAAA?rs=1&pid=ImgDetMain",
+        "img_desc"      => "Amar sonam bangla, ami tomay valobashi",
+        "btn"           => "Read More.."
+    ],
+    [
+        "img_title"     => "Menu Card 7",
+        "img"           => "https://th.bing.com/th/id/OIP.2BRS-ZmE2ayvjMuJ0gW4UAHaEo?rs=1&pid=ImgDetMain",
+        "img_desc"      => "Amar sonam bangla, ami tomay valobashi",
+        "btn"           => "Read More.."
+    ],
+
+
+];
+
+
+?>
